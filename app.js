5d8ace3879400451cec4442c84d4fb3230f78ee2
Ben <ben.ferris1@gmail.com>
2017-11-17 12:06:41 -0500

    add nodemon and heroku postbuild

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
 app.use('/users', users);
 
 // catch 404 and forward to error handler
