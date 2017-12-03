1becaedbca44e0238dd908570cad863d03ac0af7
Ben <ben.ferris1@gmail.com>
2017-12-02 23:18:04 -0500

    added bcrypt

--- models/User.js
+++ models/User.js
@@ -1,9 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: String,
   password: String
 });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) { return next() }
+
+    bcrypt.hash(user.password, 10, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    })
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(err, isMatch);
+  })
+};
 
 module.exports = mongoose.model('User', userSchema);
