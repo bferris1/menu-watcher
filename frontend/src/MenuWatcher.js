8a8feb1462f2c3223a2096173d0b917dbe64cc5c
Tyler E <emerickt@purdue.edu>
2017-12-03 20:40:39 -0500

    Added DataStepper Component to the MenuWatcher main component

--- frontend/src/MenuWatcher.js
+++ frontend/src/MenuWatcher.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import {Button, Col, Row} from 'reactstrap';
+import moment from 'moment';
+
+import {DateStepper} from './SpecialForm';
 
 export default class MenuWatcher extends Component {
 
   constructor(props){
       super (props);
+      let today = moment();
+      this.minDate = moment().startOf('day');
+      this.maxDate = moment().endOf('day').add(3, 'days');
+
       this.state = {
         userID: '',
         favFoods: [{
           itemId: '',
           itemName: ''
         }],
-        date: moment.add(-1, 'days').format('ll'),
+        date: today,
         mealTime: '',
         foodList: [{
           dinningCourt: '',
@@ -26,14 +33,21 @@ export default class MenuWatcher extends Component {
       this.setState({[e.target.name]:e.target.value});
   }
 
+  handleDateChange(newDate){
+      console.log("date: " + newDate.format('ll'));
+      this.setState({date: newDate});
+  }
+
   render() {
 
     return (
       <div>
         <h1>Purdue Menu Watcher </h1>
-        </hr>
-        <p>Today: {moment.format('ll')}</p>
-        <p>Yesterday: {this.state.date}</p>
+        <hr/>
+        <p>Today: {this.state.date.format('ll')}</p>
+        <DateStepper name={"Enter Date:"} date={this.state.date}
+          min={this.minDate} max={this.maxDate}
+          onChange={newDate => {this.handleDateChange(newDate)}} />
       </div>
     );
   }
