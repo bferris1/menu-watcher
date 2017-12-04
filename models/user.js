3de921d87cd7a2b67d24dcab6637e24f646e7691
Naman Misraraj <naman.misraraj@gmail.com>
2017-12-03 19:11:20 -0500

    added favorite model and fixed ESlint indentation

--- models/user.js
+++ models/user.js
@@ -10,20 +10,20 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   const user = this;
 
-  if (!user.isModified('password')) { return next() }
+  if (!user.isModified('password')) { return next(); }
 
-    bcrypt.hash(user.password, 10, function (err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    })
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
 });
 
 userSchema.methods.comparePassword = function (candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(err, isMatch);
-  })
+  });
 };
 
 module.exports = mongoose.model('User', userSchema);
