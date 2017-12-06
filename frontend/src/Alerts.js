53d8d2d8155fb448ea6e6a8f986755c92b3b3445
Ben <ben.ferris1@gmail.com>
2017-12-06 18:29:09 -0500

    added alerts component

---
+++ frontend/src/Alerts.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Alert} from "reactstrap";
+
+export const Alerts = (props)=> {
+
+    if (props.alerts === undefined)
+        return null;
+
+        return (
+            <div className={"alerts"}>
+                    {props.alerts.success === undefined ||
+                    <Alert color="success">
+                        {props.alerts.success}
+                    </Alert>
+                    }
+
+                    {props.alerts.danger === undefined ||
+                    <Alert color="danger">
+                        {props.alerts.danger}
+                    </Alert>
+                    }
+
+                    {props.alerts.warning === undefined ||
+                    <Alert color="warning">
+                        {props.alerts.warning}
+                    </Alert>
+                    }
+
+                    {props.alerts.info === undefined ||
+                    <Alert color="info">
+                        {props.alerts.info}
+                    </Alert>
+                    }
+                </div>
+        )
+};
+
+export default Alerts;
