b8bd990e9340ac6fef8fd04b39ac28cefb08549e
Ben <ben.ferris1@gmail.com>
2017-12-12 14:58:47 -0500

    add redux dev extension support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,10 @@ import reducer from './reducer'
 import thunk from 'redux-thunk';
 import {getUser} from './reducer/user/actions';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 
 store.dispatch(getUser());
 
