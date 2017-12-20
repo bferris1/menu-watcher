e65fbdfad31a420befa171507e99c59cb8cab687
Ben <ben.ferris1@gmail.com>
2017-12-19 21:52:12 -0500

    convert menu-checker to use promises

--- util/menu-checker.js
+++ util/menu-checker.js
@@ -1,109 +1,114 @@
 let checker = {};
 const async = require('async');
 const request = require('request');
+const rp = require('request-promise');
 
-checker.getAllMenus = function (date, callback) {
-	let baseURL = 'https://api.hfs.purdue.edu/menus/v2/locations/';
-	let locations = ['Hillenbrand', 'Earhart', 'Windsor', 'Wiley', 'Ford', 'The%20Gathering%20Place'];
-	let results = [];
+checker.getAllMenus = function (date) {
+	return new Promise((resolve, reject) => {
+		let baseURL = 'https://api.hfs.purdue.edu/menus/v2/locations/';
+		let locations = ['Hillenbrand', 'Earhart', 'Windsor', 'Wiley', 'Ford', 'The%20Gathering%20Place'];
+		let results = [];
 
-	// let dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+		async.forEachOf(locations, (location, index, cb) => {
+			let fullURL = baseURL + location + '/' + date;
 
-	async.forEachOf(locations, (location, index, cb) => {
-		let fullURL = baseURL + location + '/' + date;
-
-		let options = {
-			url: fullURL,
-			headers: {
-				'Accept': 'text/json'
-			}
-		};
-		request(options, (error, request, body) => {
-			if (error)
-				cb(error);
-			results[index] = JSON.parse(body);
-			cb();
+			let options = {
+				url: fullURL,
+				headers: {
+					'Accept': 'text/json'
+				},
+				json: true
+			};
+			request(options, (error, request, body) => {
+				if (error)
+					return cb(error);
+				results[index] = body;
+				cb();
+			});
+		}, (err) => {
+			if (err) reject(err);
+			resolve(results);
 		});
-	}, (err) => {
-		if (err)
-			console.log(err.message);
-		callback(null, results);
 	});
+
 };
 
-checker.getSearchResults = function (query, callback) {
+checker.getSearchResults = function (query) {
+
 	let baseURL = 'https://api.hfs.purdue.edu/menus/v2/items/search/';
 	// get the parameters, replace spaces with %20 and add to base url later VVV
 	// req.param()
-	let fullURL = baseURL + query; // change this later
-
+	let fullURL = baseURL + query; // change this later?
 
 	let options = {
 		url: fullURL,
 		headers: {
 			'Accept': 'text/json'
-		}
+		},
+		json: true
 	};
-	request(options, (error, request, body) => {
-		if (error)
-			callback(error);
-		else
-			callback(null, JSON.parse(body));
-	});
+	return rp(options);
 };
 
-checker.getFilteredFavorites = (menus, favorites, callback) => {
+checker.getFilteredFavorites = (menus, favorites) => {
 
-	let favoritesSet = new Set();
-	favorites.forEach(favorite => {
-		favoritesSet.add(favorite.itemID);
-	});
-	let diningCourts = [];
+	return new Promise((resolve, reject) => {
+		if (!menus || !favorites) {
+			reject('Invalid parameters.');
+		}
 
-	// todo: combine these two stages?
+		let favoritesSet = new Set();
+		favorites.forEach(favorite => {
+			favoritesSet.add(favorite.itemID);
+		});
+		let diningCourts = [];
 
-	menus.forEach((diningCourt, courtIndex) => {
-		diningCourts[courtIndex] = {name: diningCourt.Location, meals: []};
-		diningCourt.Meals.forEach((meal, mealIndex) => {
-			diningCourts[courtIndex].meals[mealIndex] = {
-				location: diningCourt.Location,
-				name: meal.Name,
-				Order: meal.Order,
-				favorites: []
-			};
-			meal.Stations.forEach(station => {
-				station.Items.forEach(item => {
-					if (favoritesSet.has(item.ID)) {
-						if (diningCourts[courtIndex].meals[mealIndex].favorites.filter(favorite => (favorite.Name === item.Name)).length === 0)
-							diningCourts[courtIndex].meals[mealIndex].favorites.push(item);
-						// meals[mealIndex].diningCourts[courtIndex].favorites.push(item);
-					}
+		// todo: combine these two stages?
+
+		menus.forEach((diningCourt, courtIndex) => {
+			diningCourts[courtIndex] = {name: diningCourt.Location, meals: []};
+			diningCourt.Meals.forEach((meal, mealIndex) => {
+				diningCourts[courtIndex].meals[mealIndex] = {
+					location: diningCourt.Location,
+					name: meal.Name,
+					Order: meal.Order,
+					favorites: []
+				};
+				meal.Stations.forEach(station => {
+					station.Items.forEach(item => {
+						if (favoritesSet.has(item.ID)) {
+							if (diningCourts[courtIndex].meals[mealIndex].favorites.filter(favorite => (favorite.Name === item.Name)).length === 0)
+								diningCourts[courtIndex].meals[mealIndex].favorites.push(item);
+							// meals[mealIndex].diningCourts[courtIndex].favorites.push(item);
+						}
+					});
 				});
 			});
 		});
-	});
 
-	let meals = [];
-	for (let i = 0; i < 4; i++) {
-		meals[i] = [];
-	}
-	diningCourts.forEach((diningCourt) => {
-		diningCourt.meals.forEach((meal) => {
-			meals[meal.Order - 1].push(meal);
+		let meals = [];
+		for (let i = 0; i < 4; i++) {
+			meals[i] = [];
+		}
+		diningCourts.forEach((diningCourt) => {
+			diningCourt.meals.forEach((meal) => {
+				meals[meal.Order - 1].push(meal);
+			});
 		});
-	});
 
-	meals.forEach(diningCourts => {
-		diningCourts.sort((first, second) => {
-			if (first.favorites.length > second.favorites.length)
-				return -1;
-			if (first.favorites.length < second.favorites.length)
-				return 1;
-			return 0;
+		meals.forEach(diningCourts => {
+			diningCourts.sort((first, second) => {
+				if (first.favorites.length > second.favorites.length)
+					return -1;
+				if (first.favorites.length < second.favorites.length)
+					return 1;
+				return 0;
+			});
 		});
+		resolve(meals);
 	});
 
-	callback(meals);
+
 };
 
 
