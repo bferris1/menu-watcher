2a2b64fe025a271c1aa62cd5b321c6c1396c96f1
Ben <ben.ferris1@gmail.com>
2017-12-21 16:10:01 -0500

    added initial webhook token auth

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,7 +1,32 @@
 let express = require('express');
 let router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const User = require('../models/user');
 const dialogActions = require('../util/dialog-actions');
 
+router.use((req, res, next) => {
+	console.log(req.headers);
+	let token = req.headers['Authorization'];
+	if (token) {
+		jwt.verify(token, config.get('jwt.secret'), function (err, decoded) {
+			if (err) {
+				return res.status(403).send({success: false, message: 'Failed to authenticate token'});
+			} else {
+				User.findOne({_id: decoded.id}).then(user => {
+					if (!user) next();
+					else {
+						req.user = decoded;
+						next();
+					}
+				}).catch(err => {
+					console.error(err);
+					return res.status(500).json({success: false, error: err});
+				});
+			}
+		});
+	} else next();
+});
 
 router.post('/', (req, res) => {
 	if (req.body.result.action === 'get_top_dining_court') {
