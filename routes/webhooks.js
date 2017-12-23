55a9df2bb81d54251864737f866aba7ec7c35ad2
Ben <ben.ferris1@gmail.com>
2017-12-23 03:59:55 -0500

    trying for different access token name

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -9,8 +9,8 @@ router.use((req, res, next) => {
 	let token;
 	console.log(req.body);
 	if (req.body.originalRequest && req.body.originalRequest.data.user && req.body.originalRequest.data.user.access_token) {
-		console.log(req.body.originalRequest.data);
-		let token = req.body.originalRequest.data.user.access_token;
+		console.log(JSON.stringify(req.body.originalRequest.data));
+		let token = req.body.originalRequest.data.user.accessToken;
 		console.log(`Got access token: ${token}`);
 	}
 
