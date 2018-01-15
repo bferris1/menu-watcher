21bd6379c32de1fac0ee679a55ef4b4b50d7a95b
Ben <ben.ferris1@gmail.com>
2018-01-15 16:16:06 -0500

    fix punctuation

--- util/dialog-actions.js
+++ util/dialog-actions.js
@@ -50,7 +50,7 @@ const formatFiltered = (filtered, mealIndex, date) => {
 	} else {
 		let best = filtered[mealIndex][0];
 		if (best.favorites.length === 0)
-			speech = `It doesn't look like any of your favorites are being served for ${best.name} ${date}`;
+			speech = `It doesn't look like any of your favorites are being served for ${best.name} ${date}.`;
 		else
 			speech = `Your top dining court for ${best.name} ${date} is ${best.location}, with ${best.favorites.length} ${best.favorites.length === 1 ? 'favorite' : 'favorites'}.`;
 	}
